01d0436e28f1083d1186121787346173028679d8
Lylyam <ines.fuentes.martinez@gmail.com>
2025-08-10 12:42:55 +0200

    feat: 2.5.step10 separateModule of courseInfo exercise

--- part2/courseInfo/src/App.jsx
+++ part2/courseInfo/src/App.jsx
@@ -1,41 +1,4 @@
-const Header = (props) => {
-  return (
-  <h1> {props.course} </h1>
-  )
-};
-
-const Part = (props) => {
-  return (
-    <p>{props.name} {props.exercises}</p>
-  );
-};
-
-const Content = (props) => {
-  return (
-    <div>
-      {props.parts.map((part) => (
-        <Part key={part.id} name={part.name} exercises={part.exercises} />
-      ))}
-    </div>
-  );
-};
-
-const Total = (props) => {
-  const total = props.parts.reduce((sum, part) => sum + part.exercises, 0);
-  return (
-    <p><b>Number of exercises {total}</b></p>
-  );
-};
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
-};
+import Course from './Course'
 
 const App = () => {
   const courses = [
